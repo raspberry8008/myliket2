66597f170385d99d99bd50b38f4d7ecdda077ca1
raspberry8008 <shmyth4@gmail.com>
2022-08-18 01:35:33 +0900

    2022.08.18

--- src/main/java/com/myliket2/myliket/MyliketApplication.java
+++ src/main/java/com/myliket2/myliket/MyliketApplication.java
@@ -1,10 +1,12 @@
 package com.myliket2.myliket;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.myliket2.myliket")
 public class MyliketApplication {
 
 	public static void main(String[] args) {
